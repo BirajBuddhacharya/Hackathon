cfb0ffe40cfe9e43a24ab86d44dd9d7f5e7ff8fb
hydrazin60 <sureshkhdk45@gmail.com>
2025-01-29 10:22:00 +0545

    Login , register , navbar , footeter setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
-
-import './App.css'
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import NavBar from "./component/NavBar";
+import Footer from "./component/Footer";
 function App() {
-
   return (
-    <>
-      <div className='text-blue-200'>Hello</div>
-    </>
-  )
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
